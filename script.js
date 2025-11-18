9091abce3e96cfcd716a5cfa2bbea827c13ad986
prosperainc <prosperflow27@gmail.com>
2025-11-18 11:23:16 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Theme switcher
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if(document.body.classList.contains('dark-mode')){
+        themeToggle.textContent = '☀️ Light Mode';
+    } else {
+        themeToggle.textContent = '🌙 Dark Mode';
+    }
+});
